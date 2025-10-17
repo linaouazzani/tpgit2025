86a30a6c5b51e4b5309d462815ae065aeeaa89b0
linaouazzani <linaouazzanichahdi@gmail.com>
2025-10-17 14:25:21 +0200

    my first git commit

---
+++ 1erTpGit.java
@@ -0,0 +1,10 @@
+
+import javax.sound.sampled.SourceDataLine;
+
+public class 1erTpGit {
+    public static void main(String[] args) {
+        
+        System.out.println("(cc mon tp)");
+    }
+    
+}
